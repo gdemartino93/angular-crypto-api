6fc915b519faacb2a6b76b4472f5e538a1a66c67
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-14 22:23:01 +0200

    installed ng2charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,8 +15,8 @@ import { HttpClientModule } from '@angular/common/http';
 import {MatTableModule} from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import {MatInputModule} from '@angular/material/input';
-import {MatSortModule} from '@angular/material/sort';
-@NgModule({
+import {MatSortModule} from '@angular/material/sort'
+import { NgChartsModule } from 'ng2-charts';@NgModule({
   declarations: [
     AppComponent,
     CoinListComponent,
@@ -35,7 +35,8 @@ import {MatSortModule} from '@angular/material/sort';
     MatTableModule,
     MatPaginatorModule,
     MatInputModule,
-    MatSortModule
+    MatSortModule,
+    NgChartsModule
 
 
   ],
--- src/app/component/coin-detail/coin-detail.component.ts
+++ src/app/component/coin-detail/coin-detail.component.ts
@@ -1,16 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/service/api.service';
-
+//lib grafici
+import { ChartConfiguration, ChartType} from 'chart.js'
+import { BaseChartDirective} from 'ng2-charts'
 @Component({
   selector: 'app-coin-detail',
   templateUrl: './coin-detail.component.html',
   styleUrls: ['./coin-detail.component.scss']
 })
 export class CoinDetailComponent implements OnInit {
+  public lineChartData: ChartConfiguration['data'] = {
 
+    datasets: [
+      {
+        data: [],
+        label: `Price Trends`,
+        backgroundColor: 'rgba(148,159,177,0.2)',
+        borderColor: '#009688',
+        pointBackgroundColor: '#009688',
+        pointBorderColor: '#009688',
+        pointHoverBackgroundColor: '#009688',
+        pointHoverBorderColor: '#009688',
+
+      }
+    ],
+    labels: []
+  };
+  public lineChartOptions: ChartConfiguration['options'] = {
+    elements: {
+      point: {
+        radius: 1
+      }
+    },
+
+    plugins: {
+      legend: { display: true },
+    }
+  };
+  public lineChartType: ChartType = 'line';
+  @ViewChild(BaseChartDirective) myLineChart !: BaseChartDirective;
   coinId : any;
   coinData !: any;
+
+
   constructor(private api : ApiService,private activatedRoute : ActivatedRoute) { }
 
   ngOnInit(): void {
