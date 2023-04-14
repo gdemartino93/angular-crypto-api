f3ac4fee62119ecf45bdb5ecd24c50ca067cd9ff
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-14 22:35:25 +0200

    ng2charts data

--- src/app/component/coin-detail/coin-detail.component.ts
+++ src/app/component/coin-detail/coin-detail.component.ts
@@ -4,6 +4,7 @@ import { ApiService } from 'src/app/service/api.service';
 //lib grafici
 import { ChartConfiguration, ChartType} from 'chart.js'
 import { BaseChartDirective} from 'ng2-charts'
+import { log } from 'console';
 @Component({
   selector: 'app-coin-detail',
   templateUrl: './coin-detail.component.html',
@@ -42,7 +43,7 @@ export class CoinDetailComponent implements OnInit {
   @ViewChild(BaseChartDirective) myLineChart !: BaseChartDirective;
   coinId : any;
   coinData !: any;
-
+  days? : number;
 
   constructor(private api : ApiService,private activatedRoute : ActivatedRoute) { }
 
@@ -51,6 +52,7 @@ export class CoinDetailComponent implements OnInit {
       this.coinId = el['id'];
     })
     this.getSingleCoin();
+    this.getGraph();
   }
 getSingleCoin(){
   this.api.getCurrencyById(this.coinId)
@@ -58,5 +60,21 @@ getSingleCoin(){
       this.coinData = res;
       console.log(this.coinData)
     })
+}
+getGraph(){
+  this.api.getGrpahicalCurrencyData(this.coinId,'EUR',1)
+  .subscribe(res => {
+   this.lineChartData.datasets[0].data = res.prices.map((el:any)=>{
+    return el[1];
+   })
+   this.lineChartData.labels = res.prices.map((el:any) => {
+    let date = new Date(el[0]);
+    let time = date.getHours() > 12 ?
+      `${date.getHours() - 12} : ${date.getMinutes()} PM ` :
+      `${date.getHours()} : ${date.getMinutes()} AM `
+    return this.days === 1 ? time : date.toLocaleDateString();
+
+   })
+  })
 }
 }
